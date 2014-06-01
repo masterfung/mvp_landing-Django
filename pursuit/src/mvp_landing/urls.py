0d6f6ba3f261a271ad6934dc727418fffb0a321c
Tsung Hung <hungmasterj@gmail.com>
2014-06-01 17:24:12 -0400

    added a thank you page

--- pursuit/src/mvp_landing/urls.py
+++ pursuit/src/mvp_landing/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'signups.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^thank-you/$', 'signups.views.thankyou', name='thankyou'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
--- pursuit/src/signups/views.py
+++ pursuit/src/signups/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, render_to_response, RequestContext
+from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,5 +12,11 @@ def home(request):
 	if form.is_valid():
 		save_it = form.save(commit=False)
 		save_it.save()
+		messages.success(request, 'Putin Berry thanks you and awaits you on the other side.')
+		return HttpResponseRedirect('/thank-you/')
 
 	return render_to_response('signup.html', locals(), context_instance=RequestContext(request))
+
+def thankyou(request):
+
+	return render_to_response('thankyou.html', locals(), context_instance=RequestContext(request))
