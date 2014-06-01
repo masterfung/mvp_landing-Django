ae8a6d3d5d793ca627d337aa1247ef39136a7484
Tsung Hung <hungmasterj@gmail.com>
2014-06-01 11:18:54 -0400

    added form

--- pursuit/src/signups/views.py
+++ pursuit/src/signups/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render, render_to_response, RequestContext
 
 # Create your views here.
 
+from .forms import SignUpForm
+
 def home(request):
+
+	form = SignUpForm()
+	
 	return render_to_response('signup.html', locals(), context_instance=RequestContext(request))
