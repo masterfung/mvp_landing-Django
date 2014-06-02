c81a43eb2f363a626a3e9b317a8b2fe2642fc230
Tsung Hung <hungmasterj@gmail.com>
2014-06-02 00:51:49 -0400

    integrated south and other tweaks

--- pursuit/src/signups/models.py
+++ pursuit/src/signups/models.py
@@ -4,6 +4,7 @@ from django.utils.encoding import smart_unicode
 # Create your models here.
 
 class SignUp(models.Model): #Sign(U)p, Django will auto space it after it encounter the 'U'
+	for_you = models.BooleanField(default=True, verbose_name = 'Are you ready to change the world?')
 	first_name = models.CharField(max_length = 120, null = True, blank = True)
 	last_name = models.CharField(max_length = 120, null = True, blank = True)
 	email = models.EmailField()
