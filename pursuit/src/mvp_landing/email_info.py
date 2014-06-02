4ae2d9128e13616ca2e4e82e36927a352f515858
Tsung Hung <hungmasterj@gmail.com>
2014-06-02 10:30:37 -0400

    email wooty

---
+++ pursuit/src/mvp_landing/email_info.py
@@ -0,0 +1,7 @@
+#for gmail -- this also work for gAPPS
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sudo@gmail.com'
+EMAIL_HOST_PASSWORD = 'kachow'
+EMAIL_PORT = 587
--- pursuit/src/signups/views.py
+++ pursuit/src/signups/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
+from django.conf import settings
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
 
 # Create your views here.
 
@@ -12,6 +14,13 @@ def home(request):
 	if form.is_valid():
 		save_it = form.save(commit=False)
 		save_it.save()
+		
+		subject = 'Putin Berry Requires Confirmation. Please Open'
+		message = 'Welcome to Putin Berry! Before we can make this official, we need you to confirm your email address. Please click the link below to do so.'
+		from_email = settings.EMAIL_HOST_USER
+		to_list = [save_it.email, settings.EMAIL_HOST_USER]
+		send_mail(subject, message, from_email, to_list, fail_silently=True)
+
 		messages.success(request, 'Putin Berry thanks you and awaits you on the other side.')
 		return HttpResponseRedirect('/thank-you/')
 
@@ -20,7 +29,7 @@ def home(request):
 def thankyou(request):
 
 	return render_to_response('thankyou.html', locals(), context_instance=RequestContext(request))
-	
+
 def aboutus(request):
 
 	return render_to_response('aboutus.html', locals(), context_instance=RequestContext(request))
